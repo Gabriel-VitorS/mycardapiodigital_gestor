e571fd02e7aefe231d58cd9157804d6446579e05
Gabriel-VitorS <gabrielvitordasilva@gmail.com>
2022-12-14 22:40:37 -0300

    Correção de middleware

--- middleware.js
+++ middleware.js
@@ -1,31 +1,29 @@
 import { jwtVerify } from 'jose'
+import { NextResponse } from 'next/server'
 
 const middleware = async (request)=>{
     const secret = await process.env.JWT_KEY
 
-    const url = request.url
-    
-    if(url.includes('/gestor')){
-
+    try {
+        
         const cookie = await request.cookies.get('jwt').value
         if(cookie == undefined){
             return NextResponse.redirect(new URL('/entrar', request.url))
         }
-        try {
-            const teste = await jwtVerify(`${cookie}`, new TextEncoder().encode(secret))
-              
-        } catch (error) {
-            const url = request.nextUrl.clone()
-            url.pathname = '/entrar'
-            return NextResponse.redirect(new URL('/entrar', request.url))
-        }
-
+        await jwtVerify(`${cookie}`, new TextEncoder().encode(secret))
+            
+    } catch (error) {
+        const url = request.nextUrl.clone()
+        url.pathname = '/entrar'
+        return NextResponse.redirect(new URL('/entrar', request.url))
     }
+
+    
 
 }
 
 export default middleware
 
 export const config = {
-    matcher: '/gestor',
+    matcher: '/gestor/:path*',
 }
